c33f14fa19a323b9fc280cc7d1decb0a86798cd6
Ahmad Nasser <79102608+POENIXMAN@users.noreply.github.com>
2025-11-18 08:49:33 +0300

    Update cuda_image_lib.h

---
+++ include/cuda_image_lib.h
@@ -0,0 +1,12 @@
+#ifndef CUDA_IMAGE_LIB_H
+#define CUDA_IMAGE_LIB_H
+
+
+#include "image_processor.h"
+#include "grayscale.h"
+#include "blur.h"
+#include "rotation.h"
+#include "batch_processor.h"
+#include "image_loader.h"
+
+#endif
